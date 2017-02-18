f81f3d499b1f468823567bbdd27b136aae4e6201
Amir Hosseinzadeh <amir.email@yahoo.com>
2017-02-18 16:30:29 +0300

    IntentService Is Changed to Service

--- app/src/main/java/com/amirhome/droidgcmlistsview/ServiceILive.java
+++ app/src/main/java/com/amirhome/droidgcmlistsview/ServiceILive.java
@@ -1,8 +1,9 @@
 package com.amirhome.droidgcmlistsview;
 
-import android.app.IntentService;
+import android.app.Service;
 import android.content.Intent;
 import android.os.Handler;
+import android.os.IBinder;
 import android.util.Log;
 
 import com.android.volley.VolleyError;
@@ -13,28 +14,28 @@ import org.json.JSONObject;
 /**
  * Created by AmirHome.com o9125 on 2/17/2017.
  */
-public class ServiceILive extends IntentService {
-    // Create the Handler object (on the main thread by default)
-    Handler handler = new Handler();
+public class ServiceILive extends Service {
 
     public ServiceILive() {
-        super("ServiceILive");
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
     }
 
     @Override
     public int onStartCommand(final Intent intent, int flags, int startId) {
         getInfo();
-
-//        handler.postDelayed(runnable, 100);
-
-        return super.onStartCommand(intent, flags, startId);
+        return START_STICKY;
     }
 
-    @Override
-    protected void onHandleIntent(Intent workIntent) {
-        // Gets data from the incoming Intent
-        // Do work here, based on the contents of dataString
-    }
     public void getInfo() {
         DetailActivity.httpRequestSyncRestaurant("info", new VolleyCallback() {
             @Override
@@ -47,13 +48,16 @@ public class ServiceILive extends IntentService {
 
                     // Refresh Action Menu
                     MainActivity.isChangedStat = true;
-                    handler.postDelayed(new Runnable() {
-                        @Override
+
+                    // Create the Handler object (on the main thread by default)
+                    Handler handler = new Handler();
+                    Runnable r = new Runnable() {
                         public void run() {
                             getInfo();
                             Log.d("AmirHomeLog", "20000 onSuccessResponse" );
                         }
-                    }, MainActivity.PERIOD_TIME_CHECKING);
+                    };
+                    handler.postDelayed(r, MainActivity.PERIOD_TIME_CHECKING);
 
                 } catch (JSONException e) {
                     MainActivity.restourantn_no = "";
@@ -64,14 +68,15 @@ public class ServiceILive extends IntentService {
                     // Refresh Action Menu
                     MainActivity.isChangedStat = true;
 
-                    //Do something after 5s = 5000ms
-                   handler.postDelayed(new Runnable() {
-                        @Override
+                    //Do something after 7s = 7000ms
+                    Handler handler = new Handler();
+                    Runnable r = new Runnable() {
                         public void run() {
                             getInfo();
-//                            Log.d("AmirHomeLog", "5000 catch run" );
+                            Log.d("AmirHomeLog", "7000 catch" );
                         }
-                    }, 5000);
+                    };
+                    handler.postDelayed(r, 7000);
                     Log.e("AmirHomeLog", "catch " + e.getMessage());
                 }
             }
@@ -85,39 +90,18 @@ public class ServiceILive extends IntentService {
                 // Refresh Action Menu
                 MainActivity.isChangedStat = true;
 
-                //Do something after 5s = 5000ms
-                handler.postDelayed(new Runnable() {
-                    @Override
+                //Do something after 6s = 6000ms
+                Handler handler = new Handler();
+                Runnable r = new Runnable() {
                     public void run() {
                         getInfo();
-//                        Log.d("AmirHomeLog", "5000 onErrorResponse" );
+                        Log.d("AmirHomeLog", "6000 onErrorResponse" );
                     }
-                }, 5000);
+                };
+                handler.postDelayed(r, 6000);
                 Log.e("AmirHomeLog", "onErrorResponse " + error.toString());
             }
         });
     }
 
-    public  Runnable sRunnableOneMinut = new Runnable() {
-        @Override
-        public void run() {
-      /* do what you need to do */
-            getInfo();
-
-      /* and here comes the "trick" */
-            handler.postDelayed(this, 100);
-        }
-    };
-
-    // Define the code block to be executed
-    private Runnable runnableCode = new Runnable() {
-        @Override
-        public void run() {
-            // Do something here on the main thread
-            Log.d("AmirHomeLog", "Called on main thread");
-            getInfo();
-            // Repeat this the same runnable code block again another 2 seconds
-            handler.postDelayed(runnableCode, 20000);
-        }
-    };
 }
