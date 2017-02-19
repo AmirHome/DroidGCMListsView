193670d69a655a571f1515990e90683e899fe9b6
Amir Hosseinzadeh <amir.email@yahoo.com>
2017-02-19 19:16:41 +0300

    Create GcmTaskService Service

--- app/src/main/java/com/amirhome/droidgcmlistsview/ServiceILive.java
+++ app/src/main/java/com/amirhome/droidgcmlistsview/ServiceILive.java
@@ -1,77 +1,33 @@
 package com.amirhome.droidgcmlistsview;
 
-import android.app.Service;
 import android.content.Intent;
 import android.os.Handler;
-import android.os.IBinder;
 import android.util.Log;
-import android.widget.Toast;
 
 import com.android.volley.VolleyError;
+import com.google.android.gms.gcm.GcmNetworkManager;
+import com.google.android.gms.gcm.GcmTaskService;
+import com.google.android.gms.gcm.TaskParams;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.Timer;
-import java.util.TimerTask;
-
 /**
  * Created by AmirHome.com o9125 on 2/17/2017.
  */
-public class ServiceILive extends Service {
-    // constant
-    public static final long NOTIFY_INTERVAL = 10 * 1000; // 10 seconds
-
-    // run on another Thread to avoid crash
-    private Handler mHandler = new Handler();
-    // timer handling
-    private Timer mTimer = null;
+public class ServiceILive extends GcmTaskService {
 
     @Override
-    public IBinder onBind(Intent intent) {
-        return null;
+    public int onRunTask(TaskParams taskParams) {
+        getInfo();
+        return GcmNetworkManager.RESULT_SUCCESS;
     }
 
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        // cancel if already existed
-        if(mTimer != null) {
-            mTimer.cancel();
-        } else {
-            // recreate new
-            mTimer = new Timer();
-        }
-        // schedule task
-        mTimer.scheduleAtFixedRate(new TimeDisplayTimerTask(), 0, MainActivity.PERIOD_TIME_CHECKING);
-
-        final Handler handler = new Handler();
-        Runnable r = new Runnable() {
-            public void run() {
-                getInfo();
-                handler.postDelayed(r,  MainActivity.PERIOD_TIME_CHECKING);
-            }
-        };
-        handler.postDelayed(r,  100);
-        return START_STICKY;
-    }
-
-    class TimeDisplayTimerTask extends TimerTask {
-
-        @Override
-        public void run() {
-            // run on another thread
-            mHandler.post(new Runnable() {
-
-                @Override
-                public void run() {
-                    // display toast
-                    Toast.makeText(getApplicationContext(), "amir",
-                            Toast.LENGTH_SHORT).show();
-//                    getInfo();
-                }
-
-            });
-        }
+    public int onStartCommand (Intent intent, int flags, int startId) {
+//        Log.d("AmirHomeLog","onStartCommand");
+        getInfo();
+        return super.onStartCommand(intent, flags, startId);
     }
 
     public void getInfo() {
@@ -86,17 +42,7 @@ public class ServiceILive extends Service {
 
                     // Refresh Action Menu
                     MainActivity.isChangedStat = true;
-
-/*                    // Create the Handler object (on the main thread by default)
-                    Handler handler = new Handler();
-                    Runnable r = new Runnable() {
-                        public void run() {
-                            getInfo();
-                            Log.d("AmirHomeLog", "20000 onSuccessResponse" );
-                        }
-                    };
-                    handler.postDelayed(r, MainActivity.PERIOD_TIME_CHECKING);*/
-                    Log.d("AmirHomeLog", "20000 onSuccessResponse" );
+//                    Log.d("AmirHomeLog", "20000 onSuccessResponse");
 
                 } catch (JSONException e) {
                     MainActivity.restourantn_no = "";
@@ -112,7 +58,7 @@ public class ServiceILive extends Service {
                     Runnable r = new Runnable() {
                         public void run() {
                             getInfo();
-                            Log.d("AmirHomeLog", "7000 catch" );
+                            Log.d("AmirHomeLog", "7000 catch");
                         }
                     };
                     handler.postDelayed(r, 7000);
@@ -126,6 +72,7 @@ public class ServiceILive extends Service {
                 MainActivity.restourantn_title = "";
                 MainActivity.open_status = "";
                 MainActivity.service_status = "";
+
                 // Refresh Action Menu
                 MainActivity.isChangedStat = true;
 
@@ -134,7 +81,7 @@ public class ServiceILive extends Service {
                 Runnable r = new Runnable() {
                     public void run() {
                         getInfo();
-                        Log.d("AmirHomeLog", "6000 onErrorResponse" );
+                        Log.d("AmirHomeLog", "6000 onErrorResponse");
                     }
                 };
                 handler.postDelayed(r, 6000);
