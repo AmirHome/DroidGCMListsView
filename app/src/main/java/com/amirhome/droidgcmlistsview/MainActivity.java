d2020bcb2d25826208301f84b722039df9404159
Amir Hosseinzadeh <amir.email@yahoo.com>
2017-02-23 19:02:17 +0300

    set version 0.0.7.05

--- app/src/main/java/com/amirhome/droidgcmlistsview/MainActivity.java
+++ app/src/main/java/com/amirhome/droidgcmlistsview/MainActivity.java
@@ -49,9 +49,9 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-    //                final static String DB_URL = "https://eat2donate-9f194.firebaseio.com/";
+//                    final static String DB_URL = "https://eat2donate-9f194.firebaseio.com/";
     final static String DB_URL = "https://eat2donatemap.firebaseio.com/";
-    public static final String APP_VERSION = "0.0.7.01";
+    public static final String APP_VERSION = "0.0.7.05";
 
     public static final String DateTimeFormat = "dd.MM.yyyy HH:mm:ss";
     public static final int DelayedMili = 180000;// 3 x 60 x 1000 = 180000 mis
